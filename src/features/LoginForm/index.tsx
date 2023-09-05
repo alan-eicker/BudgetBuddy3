1113559e80c4c4e03bd62ee2f79ac917b9fb5dad
Alan Eicker <alaneicker@gmail.com>
2023-09-05 08:08:22 -0500

    refactoring

--- src/features/LoginForm/index.tsx
+++ src/features/LoginForm/index.tsx
@@ -1,21 +1,17 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
 import LoadingButton from '@mui/lab/LoadingButton';
 import BrandLogo from '@/components/BrandLogo';
-import ContentSection from '@/components/ContentSection';
-
-import { useAuth } from '@/hooks/useAuth';
+import { useLogin } from '@/features/LoginForm/useLogin';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import styles from './LoginForm.module.scss';
 
 const LoginForm = (): JSX.Element => {
-  const router = useRouter();
-  const { authError, login } = useAuth();
+  const { loginError, login } = useLogin();
   const [isloading, setIsLoading] = useState(false);
 
   const initialValues = {
@@ -37,55 +33,54 @@ const LoginForm = (): JSX.Element => {
       login();
     },
   });
+
   return (
-    <ContentSection maxWidth={500}>
-      <form
-        className={styles.loginFormContainer}
-        onSubmit={handleSubmit}
-        noValidate
+    <form onSubmit={handleSubmit} noValidate>
+      <Typography component="div" textAlign="center">
+        <BrandLogo size={70} />
+        <Typography component="h1" variant="h5" fontWeight={600}>
+          BudgetBuddy
+        </Typography>
+      </Typography>
+      {loginError && (
+        <Alert variant="outlined" severity="error">
+          Invalid user credentials
+        </Alert>
+      )}
+      <TextField
+        required
+        label="Username"
+        name="username"
+        onChange={handleChange}
+        value={values.username}
+        {...(!!(errors.username && touched.username) && {
+          error: true,
+          helperText: errors.username,
+        })}
+        autoComplete="no"
+      />
+      <TextField
+        required
+        type="password"
+        label="Password"
+        name="password"
+        onChange={handleChange}
+        value={values.password}
+        {...(!!(errors.password && touched.password) && {
+          error: true,
+          helperText: errors.password,
+        })}
+      />
+      <LoadingButton
+        color="primary"
+        variant="contained"
+        type="submit"
+        size="large"
+        loading={isloading}
       >
-        <div>
-          <BrandLogo size={70} />
-          <h1 className={styles.loginFormHeading}>BudgetBuddy</h1>
-        </div>
-        {authError && (
-          <Alert variant="outlined" severity="error">
-            Invalid user credentials
-          </Alert>
-        )}
-        <TextField
-          required
-          label="Username"
-          name="username"
-          onChange={handleChange}
-          value={values.username}
-          {...(!!(errors.username && touched.username) && {
-            error: true,
-            helperText: errors.username,
-          })}
-          autoComplete="no"
-        />
-        <TextField
-          required
-          type="password"
-          label="Password"
-          name="password"
-          onChange={handleChange}
-          value={values.password}
-          {...(!!(errors.password && touched.password) && {
-            error: true,
-            helperText: errors.password,
-          })}
-        />
-        <LoadingButton
-          color="primary"
-          variant="contained"
-          type="submit"
-          size="large"
-          loading={isloading}
-        >
-          Log In
-        </LoadingButton>
+        Log In
+      </LoadingButton>
+      <Typography component="div" textAlign="center">
         <p>
           <Button
             size="small"
@@ -106,8 +101,8 @@ const LoginForm = (): JSX.Element => {
             Create an Account
           </Button>
         </p>
-      </form>
-    </ContentSection>
+      </Typography>
+    </form>
   );
 };
 
