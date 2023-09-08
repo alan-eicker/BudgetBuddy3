aa7dca023ddb595da5b3fd5e2f49257b86ea9e0f
Alan Eicker <alaneicker@gmail.com>
2023-09-08 14:47:38 -0500

    Adds tending widget

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,7 +4,7 @@ import Box, { BoxProps } from '@mui/material/Box';
 import styles from './Card.module.scss';
 
 interface CardProps extends BoxProps {
-  head: string | JSX.Element;
+  head?: string | JSX.Element;
   children: React.ReactNode;
   height?: number | string;
   hasError?: boolean;
@@ -19,7 +19,7 @@ const Card = ({ head, children, height, hasError, ...boxProps }: CardProps) => (
     width="100%"
     {...boxProps}
   >
-    <Box className={styles.head}>{head}</Box>
+    {head && <Box className={styles.head}>{head}</Box>}
     <Box className={styles.body}>{children}</Box>
   </Box>
 );
--- src/components/SpendingSnapshot/index.tsx
+++ src/components/SpendingSnapshot/index.tsx
@@ -2,6 +2,10 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import ArrowDownwardRoundedIcon from '@mui/icons-material/ArrowDownwardRounded';
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
 import Card from '../Card';
 
 interface SpendingSnapshotProps {
@@ -20,6 +24,13 @@ const SpendingSnapshot = ({
       </Typography>
     }
   >
+    <Box textAlign="center" color="#8bc34a">
+      <ArrowDownwardRoundedIcon sx={{ fontSize: 50 }} />
+      <Box marginTop={-1}>Trending 5% Lower than last period</Box>
+    </Box>
+    <Box marginTop={2.5} marginBottom={2.5}>
+      <Divider />
+    </Box>
     <List disablePadding dense>
       {items.map(([label, value]) => (
         <ListItem key={label} className="text-center" disablePadding>
