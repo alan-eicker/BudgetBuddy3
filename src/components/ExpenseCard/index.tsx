9f15645c8b5949dc4c051781bda8793536ca8f62
Alan Eicker <alaneicker@gmail.com>
2023-09-08 15:14:12 -0500

    updates info icon for expense note

--- src/components/ExpenseCard/index.tsx
+++ src/components/ExpenseCard/index.tsx
@@ -2,7 +2,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Switch from '@mui/material/Switch';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import Card from '@/components/Card';
 import { Expense } from '@/graphql/generated/graphql';
 import { ReactElement } from 'react';
@@ -59,7 +59,7 @@ const ExpenseCard = ({
     </Box>
     {note && (
       <Box display="flex" alignItems="center" marginTop={1}>
-        <ErrorOutlineOutlinedIcon
+        <InfoOutlinedIcon
           color="info"
           fontSize="small"
           sx={{ marginRight: 0.5 }}
