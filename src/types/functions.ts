a51262e337389f017d95055573bd085fd9afbfa1
Alan Eicker <alaneicker@gmail.com>
2023-09-17 14:32:55 -0500

    Updates UnknownFuntionType type

--- src/types/functions.ts
+++ src/types/functions.ts
@@ -1,9 +1,1 @@
-import { Dispatch, SetStateAction } from 'react';
-
-export type UnknownFuntionType<T> = (
-  ...args: any[]
-) => any | Promise<T> | Dispatch<SetStateAction<T>>;
-
-export type PromiseReturnType<T> = Promise<T>;
-
-export type SetStateActionType<T> = Dispatch<SetStateAction<T>>;
+export type UnknownFuntionType<T> = (...args: any[]) => T;
