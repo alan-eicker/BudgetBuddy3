3f4b7d3ecb75c5a42bd943d07d5aa58e5186a67f
Alan Eicker <alaneicker@gmail.com>
2023-09-21 06:45:06 -0500

    Sorts expense groups in descending order

--- src/graphql/resolvers/queries/expenseGroups.ts
+++ src/graphql/resolvers/queries/expenseGroups.ts
@@ -5,7 +5,12 @@ import {
 import ExpenseGroupModel from '@/database/models/expenseGroup';
 
 export async function getAllExpenseGroups(): Promise<ExpenseGroup[]> {
-  return await ExpenseGroupModel.find();
+  const expenseGroups = await ExpenseGroupModel.find();
+  expenseGroups.sort(
+    (a: ExpenseGroup, b: ExpenseGroup) =>
+      new Date(b.startDate).valueOf() - new Date(a.startDate).valueOf(),
+  );
+  return expenseGroups;
 }
 
 export async function getExpenseGroupById(
