f0b990920cef5ba36467cba215c26e6f3de30ee9
Alan Eicker <alaneicker@gmail.com>
2023-09-23 13:33:43 -0500

    updates create account form layout

--- src/features/RegisterUserForm/index.tsx
+++ src/features/RegisterUserForm/index.tsx
@@ -1,27 +1,36 @@
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import BrandLogo from '@/components/BrandLogo';
 
 export default function RegisterUserForm() {
   return (
     <form>
-      <Typography
-        sx={{ marginBottom: 2, textAlign: 'center' }}
-        variant="h5"
-        component="div"
-      >
-        <BrandLogo size={60} />
-        <Typography component="h1" variant="h5">
-          Create an Account
+      <Box padding={2.5}>
+        <Typography
+          sx={{ marginBottom: 2, textAlign: 'center' }}
+          variant="h5"
+          component="div"
+        >
+          <BrandLogo size={60} />
+          <Typography component="h1" variant="h5">
+            Create an Account
+          </Typography>
         </Typography>
-      </Typography>
-      <TextField fullWidth required label="Email" name="email" />
-      <TextField fullWidth required label="Username" name="username" />
-      <TextField fullWidth required label="Password" name="password" />
-      <Button variant="contained" size="large" fullWidth>
-        Create User
-      </Button>
+        <Box marginBottom={2}>
+          <TextField fullWidth required label="Email" name="email" />
+        </Box>
+        <Box marginBottom={2}>
+          <TextField fullWidth required label="Username" name="username" />
+        </Box>
+        <Box marginBottom={2}>
+          <TextField fullWidth required label="Password" name="password" />
+        </Box>
+        <Button variant="contained" size="large" fullWidth>
+          Create User
+        </Button>
+      </Box>
     </form>
   );
 }
