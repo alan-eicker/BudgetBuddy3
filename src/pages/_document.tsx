f4fbeb4733c48b86c9f729051618e44a12e50cf1
Alan Eicker <alaneicker@gmail.com>
2023-09-24 06:54:26 -0500

    fixes ts error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          crossOrigin=""
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&family=Raleway:wght@400;500;600;700;800;900&display=swap"
