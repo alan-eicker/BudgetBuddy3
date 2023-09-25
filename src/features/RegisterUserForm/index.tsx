cf614322496573a2b9525f7beb4a0e70de05c78b
Alan Eicker <alaneicker@gmail.com>
2023-09-25 06:24:36 -0500

    register user form validation

--- src/features/RegisterUserForm/index.tsx
+++ src/features/RegisterUserForm/index.tsx
@@ -1,12 +1,39 @@
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import BrandLogo from '@/components/BrandLogo';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export default function RegisterUserForm() {
+  const initialValues = {
+    email: '',
+    username: '',
+    password: '',
+  };
+
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .matches(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        'Invalid email format',
+      )
+      .required('Email is required'),
+    username: yup.string().required('Username is required'),
+    password: yup.string().required('Password is required'),
+  });
+
+  const { errors, touched, values, handleChange, handleSubmit, isSubmitting } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: (formData) => {},
+    });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit} noValidate>
       <Box padding={4}>
         <Typography
           sx={{ marginBottom: 2, textAlign: 'center' }}
@@ -19,17 +46,57 @@ export default function RegisterUserForm() {
           </Typography>
         </Typography>
         <Box marginBottom={2}>
-          <TextField fullWidth required label="Email" name="email" />
+          <TextField
+            fullWidth
+            required
+            label="Email"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            {...(!!(errors.email && touched.email) && {
+              error: true,
+              helperText: errors.email,
+            })}
+          />
         </Box>
         <Box marginBottom={2}>
-          <TextField fullWidth required label="Username" name="username" />
+          <TextField
+            fullWidth
+            required
+            label="Username"
+            name="username"
+            onChange={handleChange}
+            value={values.username}
+            {...(!!(errors.username && touched.username) && {
+              error: true,
+              helperText: errors.username,
+            })}
+          />
         </Box>
         <Box marginBottom={2}>
-          <TextField fullWidth required label="Password" name="password" />
+          <TextField
+            fullWidth
+            required
+            type="password"
+            label="Password"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            {...(!!(errors.password && touched.password) && {
+              error: true,
+              helperText: errors.password,
+            })}
+          />
         </Box>
-        <Button variant="contained" size="large" fullWidth>
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          size="large"
+          loading={isSubmitting}
+          fullWidth
+        >
           Create User
-        </Button>
+        </LoadingButton>
       </Box>
     </form>
   );
